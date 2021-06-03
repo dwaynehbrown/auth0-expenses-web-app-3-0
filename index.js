f73fe42a44db0cf41b0d9fa969eedf3b1eef40f1
Dwayne Brown <dwayne@pebr.co.uk>
2021-06-03 16:33:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -74,20 +74,15 @@ app.get("/user", requiresAuth(), async (req, res) => {
 });
 
 app.get("/expenses", requiresAuth(), async (req, res, next) => {
-  try {
-    const { token_type, access_token } = req.oidc.accessToken;
-    const expenses = await axios.get(`${API_URL}/reports`, {
-       headers: {
-        Authorization: `${token_type} ${access_token}`,
-       },
-    });      
-    res.render("expenses", {
-      user: req.oidc && req.oidc.user,
-      expenses: expenses.data,
-    });
-  } catch (err) {
-    next(err);
-  }
+ try {
+  const expenses = await axios.get(`${API_URL}/reports`);
+  res.render("expenses", {
+   user: req.oidc && req.oidc.user,
+   expenses: expenses.data,
+  });
+ } catch (err) {
+  next(err);
+ }
 });
 
 // catch 404 and forward to error handler
